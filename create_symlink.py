df8d87a94e16922a248462acb5f51657ae4fef1e
Sakumatti O Luukkonen <sakumatti.luukkonen@cs.helsinki.fi>
2009-03-07 16:13:35 +0200

    Added a script to alternatively symlink the files.

---
+++ create_symlink.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import os
+home = os.path.abspath(os.environ['HOME'])
+path = os.path.join(home, 'src/configs') 
+excludes = ['.gitignore', '.config', 'create_symlinks.py'] 
+for f in os.listdir(path):
+    if f not in excludes:
+        dst = os.path.join(home, f)
+        src = os.path.abspath(f)
+        try:
+            print "Symlinking %s to %s" % (src, dst)
+            os.symlink(src, dst)
+        except Exception, msg:
+            print "Failed to symlink %s to %s " % (src, dst)
+            print msg
