c90299426cb6ba891e270efb49a9dbf9278c30c9
Sakumatti Luukkonen <saku@hel.(none)>
2009-05-05 21:05:16 +0300

    Fixed typo.

--- create_symlink.py
+++ create_symlink.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
 import os
 home = os.path.abspath(os.environ['HOME'])
-path = os.path.join(home, 'Development//configs') 
-excludes = ['.gitignore', '.config', 'create_symlinks.py', 'bin',
-            'copy_config.sh'] 
+path = os.path.join(home, 'Downloads/configs') 
+excludes = ['.gitignore', '.config', 'create_symlink.py', 'copy_config.sh'] 
 for f in os.listdir(path):
     if f not in excludes:
         dst = os.path.join(home, f)
