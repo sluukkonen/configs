e20cfe41fe6246706ad1a081d81e39f3852ef7bf
Sakumatti Luukkonen <saku@thor.(none)>
2009-05-19 01:04:33 +0300

    Added xmonad config.

---
+++ .xmonad/xmonad.hs
@@ -0,0 +1,14 @@
+-- Xmonad settings by sluukkonen (sluukkonen@github)
+-- Last update: Sat May 16 13:04:40 EEST 2009
+
+import XMonad
+import XMonad.Config.Gnome
+
+import XMonad.Layout.NoBorders
+
+main = xmonad $ gnomeConfig 
+		{ 
+			modMask = mod4Mask,
+			terminal = "terminator",
+			layoutHook = smartBorders $ layoutHook gnomeConfig
+		}
