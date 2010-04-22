50111e17a24a6e57c8f51ea5d5cbe910cb25603d
Sakumatti <sakumatti.luukkonen@cs.helsinki.fi>
2010-04-22 18:14:54 +0300

    Add a full screen manageHook to the xmonad configuration. This seems to fix the problem with full screen flash.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,9 +1,13 @@
 import XMonad
 import XMonad.Config.Gnome
 import XMonad.Layout.NoBorders
+import XMonad.Hooks.ManageHelpers (isFullscreen, doFullFloat)
 
 main = xmonad $ gnomeConfig 
-		{ 
-			terminal = "gnome-terminal",
-			layoutHook = smartBorders $ layoutHook gnomeConfig
-		}
+    { 
+	    terminal   = "gnome-terminal",
+        layoutHook = smartBorders $ layoutHook gnomeConfig,
+        manageHook = composeAll
+            [ manageHook gnomeConfig,
+              isFullscreen --> doFullFloat ]
+    }
