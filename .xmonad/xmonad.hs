9e1f890829455a765050ad39e532d97125839864
Sakumatti Luukkonen <sakumatti.luukkonen@eficode.fi>
2012-01-05 15:47:41 +0200

    Ignore unity panel & launcer.

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -3,11 +3,14 @@ import XMonad.Config.Gnome
 import XMonad.Layout.NoBorders
 import XMonad.Hooks.ManageHelpers (isFullscreen, doFullFloat)
 
-main = xmonad $ gnomeConfig 
-    { 
+main = xmonad $ gnomeConfig
+    {
         terminal   = "gnome-terminal",
         layoutHook = smartBorders $ layoutHook gnomeConfig,
         manageHook = composeAll
             [ manageHook gnomeConfig,
-              isFullscreen --> doFullFloat ]
+              isFullscreen --> doFullFloat,
+              className =? "Unity-2d-panel" --> doIgnore,
+              className =? "Unity-2d-launcher" --> doIgnore
+            ]
     }
