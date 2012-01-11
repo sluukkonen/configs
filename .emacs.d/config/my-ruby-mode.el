7b56a3b3e79c888cb1c5d2e58ff285138c7540ba
Sakumatti Luukkonen <sakumatti.luukkonen@eficode.com>
2012-01-11 15:41:01 +0200

    Prevent ruby-mode from adding encoding comments to source files.

--- .emacs.d/config/my-ruby-mode.el
+++ .emacs.d/config/my-ruby-mode.el
@@ -3,4 +3,5 @@
             (ruby-electric-mode t)
             (substitute-key-definition 'ruby-electric-curlies nil ruby-mode-map)
             (substitute-key-definition 'ruby-electric-matching-char nil ruby-mode-map)
-            (substitute-key-definition 'ruby-electric-close-matching-char nil ruby-mode-map)))
+            (substitute-key-definition 'ruby-electric-close-matching-char nil ruby-mode-map)
+            (setq ruby-insert-encoding-magic-comment nil)))
