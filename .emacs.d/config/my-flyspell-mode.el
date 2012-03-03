655c02eb6590b312ecd86a373376df30b9ea6765
Sakumatti Luukkonen <sakumatti.luukkonen@gmail.com>
2012-03-03 11:46:03 +0200

    Delete flyspell mode from default config.

--- .emacs.d/config/my-flyspell-mode.el
+++
@@ -1,5 +0,0 @@
-(add-hook 'text-mode-hook 'turn-on-flyspell)
-(add-hook 'c-mode-common-hook 'flyspell-prog-mode)
-(add-hook 'ruby-mode-hook 'flyspell-prog-mode)
-(add-hook 'emacs-lisp-mode-hook 'flyspell-prog-mode)
-(add-hook 'scheme-mode-hook 'flyspell-prog-mode)
