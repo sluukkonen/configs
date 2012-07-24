0116fcfd4cd4fbb7a32309865a363bb374eb718f
Sakumatti Luukkonen <sakumatti.luukkonen@gmail.com>
2012-07-24 20:50:36 +0300

    Don't symlink bin directory from configs to $HOME/bin.
    Instead, create $HOME/bin if it doesn't already exists and symlink
    individual files from bin to $HOME/bin.

--- create_symlink.py
+++ create_symlink.py
@@ -3,15 +3,28 @@
 import os
 
 home = os.path.abspath(os.environ['HOME'])
-path = os.path.dirname(__file__)
-excludes = ['.gitignore', '.git', '.gitmodules', os.path.basename(__file__)]
+path = os.path.abspath(os.path.dirname(__file__))
 
-for f in os.listdir(path):
-    if f not in excludes:
-        src = os.path.abspath(f)
-        dst = os.path.join(home, f)
-        try:
-            print "Symlinking %s to %s" % (src, dst)
-            os.symlink(src, dst)
-        except Exception, msg:
-            print "Failed to symlink %s to %s: %s" % (src, dst, msg)
+bin = os.path.join(home, 'bin')
+local_bin = os.path.join(path, 'bin')
+
+excludes = ['.gitignore', '.git', '.gitmodules', os.path.basename(__file__), 'bin']
+
+def link_directory(srcdir, dstdir):
+    for f in os.listdir(srcdir):
+        if f not in excludes:
+            src = os.path.abspath(os.path.join(srcdir, f))
+            dst = os.path.join(dstdir, f)
+            try:
+                os.symlink(src, dst)
+                print "ln -s %s %s" % (src, dst)
+            except Exception, msg:
+                pass
+
+if not os.path.exists(bin):
+    os.mkdir(bin)
+    print "mkdir %s" % bin
+
+link_directory(path, home)
+link_directory(local_bin, bin)
+
