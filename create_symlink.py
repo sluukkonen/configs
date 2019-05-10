d50e77e6d9683e931010572e8437f3719e6064d3
Sakumatti Luukkonen <sakumatti.luukkonen@reaktor.com>
2019-05-10 22:03:45 +0300

    Simplify create-symlink.py

--- create_symlink.py
+++ create_symlink.py
@@ -5,26 +5,32 @@ import os
 home = os.path.abspath(os.environ['HOME'])
 path = os.path.abspath(os.path.dirname(__file__))
 
-bin = os.path.join(home, 'bin')
-local_bin = os.path.join(path, 'bin')
+excludes = ['.gitignore', '.git', '.gitmodules', os.path.basename(__file__), 'Brewfile']
 
-excludes = ['.gitignore', '.git', '.gitmodules', os.path.basename(__file__), 'bin', 'Brewfile']
+def ensure_directory(dir):
+    relpath = os.path.relpath(dir, path)
+    targetpath = os.path.join(home, relpath)
+    if not os.path.exists(targetpath):
+        os.mkdir(targetpath)
+        print "mkdir %s" % targetpath
 
-def link_directory(srcdir, dstdir):
-    for f in os.listdir(srcdir):
-        if f not in excludes:
-            src = os.path.abspath(os.path.join(srcdir, f))
-            dst = os.path.join(dstdir, f)
-            try:
-                os.symlink(src, dst)
-                print "ln -s %s %s" % (src, dst)
-            except Exception, msg:
-                pass
+def link_file(file):
+    relpath = os.path.relpath(file, path)
+    abspath = os.path.abspath(os.path.join(home, relpath)) 
+    try:
+        os.symlink(file, abspath)
+        print "ln -s %s %s" % (file, abspath)
+    except:
+       pass
 
-if not os.path.exists(bin):
-    os.mkdir(bin)
-    print "mkdir %s" % bin
+def link_directory(dir):
+    for file in os.listdir(dir):
+        if file not in excludes:
+	    abspath = os.path.abspath(os.path.join(dir, file))
+            if os.path.isfile(abspath):
+                link_file(abspath)
+            elif os.path.isdir(abspath):
+                ensure_directory(abspath)
+                link_directory(abspath)
 
-link_directory(path, home)
-link_directory(local_bin, bin)
-
+link_directory(path)
