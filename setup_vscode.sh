d17839b265a951b194546c44733b9e03171a6bb0
Sakumatti Luukkonen <sakumatti.luukkonen@gmail.com>
2020-02-10 21:00:41 +0200

    Add VS Code config

---
+++ setup_vscode.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+CONFDIR="$(pwd)/.config/Code/User"
+EXTENSIONS="$CONFDIR/extensions.txt"
+TMPFILE=$(mktemp)
+MACOS_CONFDIR="$HOME/Library/Application Support/Code/User"
+SETTINGS_FILE=settings.json
+
+( code --list-extensions ; cat "$EXTENSIONS" ) | sort -u > "$TMPFILE"
+mv "$TMPFILE" "$EXTENSIONS"
+
+xargs -L 1 code --install-extension < "$EXTENSIONS"
+
+if [[ "$(uname)" == "Darwin" ]]; then
+  mkdir -p "$MACOS_CONFDIR"
+  ln -fs "$CONFDIR/$SETTINGS_FILE" "$MACOS_CONFDIR/$SETTINGS_FILE"
+fi
