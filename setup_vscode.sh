712105c1f24686d817c6bd40133f6c6473dd0ab5
Sakumatti Luukkonen <sakumatti.luukkonen@gmail.com>
2020-02-11 11:56:45 +0200

    setup_vscode.sh: Ignore case when sorting

--- setup_vscode.sh
+++ setup_vscode.sh
@@ -8,7 +8,7 @@ TMPFILE=$(mktemp)
 MACOS_CONFDIR="$HOME/Library/Application Support/Code/User"
 SETTINGS_FILE=settings.json
 
-( code --list-extensions ; cat "$EXTENSIONS" ) | sort -u > "$TMPFILE"
+( code --list-extensions ; cat "$EXTENSIONS" ) | sort -fu > "$TMPFILE"
 mv "$TMPFILE" "$EXTENSIONS"
 
 xargs -n 1 code --install-extension < "$EXTENSIONS"
