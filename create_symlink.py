558e18c4d98f61be80277233e265cb37d3bb07ee
Sakumatti Luukkonen <sakumatti.luukkonen@gmail.com>
2026-09-07 12:42:18 +0300

    Replace config files under home automatically with symlinks, when it's safe to do

--- create_symlink.py
+++ create_symlink.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
+import filecmp
 import os
+import uuid
 
 home = os.path.abspath(os.environ['HOME'])
 path = os.path.abspath(os.path.dirname(__file__))
@@ -20,12 +22,33 @@ def ensure_directory(dir):
 def link_file(file):
     relpath = os.path.relpath(file, path)
     abspath = os.path.abspath(os.path.join(home, relpath))
+
+    if os.path.lexists(abspath):
+        if os.path.islink(abspath):
+            return
+
+        if os.path.isfile(abspath) and filecmp.cmp(file, abspath, shallow=False):
+            # Create the link before replacing the file so a failure cannot
+            # leave the user without their existing config.
+            temporary_path = "%s.symlink-%s" % (abspath, uuid.uuid4().hex)
+            try:
+                os.symlink(file, temporary_path)
+                os.replace(temporary_path, abspath)
+                print("ln -s %s %s (replaced identical file)" % (file, abspath))
+            except OSError as error:
+                if os.path.lexists(temporary_path):
+                    os.unlink(temporary_path)
+                print("Could not replace %s: %s" % (abspath, error))
+            return
+
+        print("%s already exists!" % abspath)
+        return
+
     try:
         os.symlink(file, abspath)
         print("ln -s %s %s" % (file, abspath))
-    except:
-        if not os.path.islink(abspath):
-            print("%s already exists!" % abspath)
+    except OSError as error:
+        print("Could not link %s: %s" % (abspath, error))
 
 
 def link_directory(dir):
